c7889945395a748995042f3ca153463a25264eaf
kmne68 <kmne68@gmail.com>
2014-09-04 00:46:41 -0500

    Initial commit of RockPaperScissors.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String userChoice;
+            Console.WriteLine("Please make your selection, Rock, Paper or Scissors");
+            Console.WriteLine("Enter 'R' for Rock, 'P' for Paper or 'S' for Scissors");
+            userChoice = Console.ReadLine().Substring(0,1).ToUpper();
+
+            if (!userChoice.Equals('R') || !userChoice.Equals('S') || !userChoice.Equals('P'))
+            {
+                Console.WriteLine("Please enter a valid choice.");
+            }
+            else
+            {
+                Console.WriteLine((userChoice));
+            }
+                
+            RockPaperScissors RPS = new RockPaperScissors(userChoice);
+
+            Console.WriteLine(RPS.determineWinner());
+            Console.ReadLine();
+        }
+        
+    }
+    
+}
---
+++ RockPaperScissors.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLoop
+{
+    class RockPaperScissors
+    {
+
+        private int playerChoice;
+        private int computerChoice;
+
+        public RockPaperScissors(string playerThrow)
+        {
+            playerChoice = playerChoiceToInt(playerThrow);
+            computerChoice = computerThrow();
+        }
+
+        public int playerChoiceToInt(string choice)
+        {
+            if (playerChoice.Equals("R"))
+                return 0;
+            else if (playerChoice.Equals("P"))
+                return 3;
+            else
+                return 6;
+        }
+
+        public int computerThrow()
+        {
+            Random rnd = new Random();
+            int randomThrow = rnd.Next(1, 4);
+            return randomThrow;
+        }
+
+        public string determineWinner()
+        {
+            int result = playerChoice + computerChoice;
+            string winner;
+            switch (result)
+            {
+                case 0:
+                case 5:
+                case 10:
+                    winner = "tie";
+                    break;
+                case 1:
+                case 2:
+                case 6:
+                    winner = "computer";
+                    break;
+                default:
+                    winner = "player";
+                    break;
+            }
+            return winner;   
+        }
+    }
+}
